8ec68fb7bfa0e2f3264ba9094f24638b28c53de6
Michael <michael.patel94@gmail.com>
2022-10-08 18:57:07 -0400

    Refactoring

---
+++ table_app/README.md
@@ -0,0 +1,49 @@
+# NBA-Web
+## Overview
+A simple web app that displays tables
+
+## Dataset
+* Dataset courtesy of [Justinas Cirtautas](https://www.kaggle.com/datasets/justinas/nba-players-data)
+
+## Instructions
+### MongoDB server (over Docker)
+1. Start Docker Desktop
+2. Build docker image (e.g. called `mongodb`)
+```
+docker build -t <image name> <path to Dockerfile>
+```
+
+2. Start MongoDB server
+```
+docker run --name <container name> -p <host port>:<container port> -v <host volume file path>:/data/db <image name>
+```
+
+3. Open MongoDB Compass
+4. Connect to the running instance
+5. Using MongoDB Compass, import the csv data located in `data`
+
+### Web app
+#### Virtual environment
+1. Create a new virtual environment using pipenv (navigate to specific directory)
+```
+pipenv --python 3
+```
+
+2. Activate the new environment (used Windows)
+```
+pipenv shell
+```
+
+3. Install python packages into the environment
+```
+pipenv install -r .\requirements.txt
+```
+
+#### Start web app
+1. Start the server (in debug mode)
+```
+uvicorn run:app --reload
+```
+
+### Test Endpoints
+1. Used Postman to test endpoints
---
+++ table_app/run.py
@@ -0,0 +1,77 @@
+"""
+Michael Patel
+August 2022
+
+Project description:
+
+File description:
+
+"""
+################################################################################
+# Imports
+from datetime import datetime
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, FileResponse
+from loguru import logger
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+from pydantic import BaseModel
+from pymongo import MongoClient
+from typing import Optional
+
+# from other modules in project
+import config
+
+
+################################################################################
+# Models
+class Player(BaseModel):
+    name: str
+    seasons: pd.Series
+    points: pd.Series
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+################################################################################
+# Setup
+app = FastAPI()
+client = MongoClient(host=config.HOST)
+db = client[config.DATABASE]
+collection = db[config.COLLECTION]
+
+
+################################################################################
+# Routes
+@app.get("/databases")
+async def get_databases():
+    return client.list_database_names()
+
+
+@app.get("/player")
+async def get_player(first_name: str, last_name: str):
+    query = {
+        "Player": f'{first_name} {last_name}'
+    }
+    results = collection.find(query, {"_id": False})
+    df = pd.DataFrame(list(results))
+    # logger.info(f'{df}')
+
+    p = Player(
+        name=f'{first_name} {last_name}',
+        seasons=df["Season"],
+        points=df["PTS"].astype(float)
+    )
+
+    # plot data
+    plt.plot(p.seasons, p.points)
+    filename = "plot.png"
+    filepath = os.path.join(config.TMP_DIR, filename)
+    plt.savefig(filepath)
+    # return HTMLResponse(content=df.to_html())
+    return FileResponse(filepath)
+
+
+################################################################################
